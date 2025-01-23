5a2134d9500c17c07ecdf86d53e5d87e9ec9ab7d
sh4rleneb <banawasharlene8@gmail.com>
2025-01-23 10:47:47 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let clickCount = 0;
+const colors = ['#F8BBD0', '#B2E0D4'];
+
 function changeBGcolor() {
-    document.body.style.backgroundColor = '#F6F5F2';
+    if (clickCount < 6) { 
+        document.body.style.backgroundColor = colors[clickCount % 2];
+        clickCount++;
+    } else {
+        clickCount = 0;
+    }
 }
 
 document.getElementById('colorButton').addEventListener('click', changeBGcolor);
